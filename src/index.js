50b1674299736dd5c05a393727f2cb872240af26
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2018-02-13 16:30:26 +0100

    fixing conflict

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 export { default as Topbar } from './Topbar'
 export { default as Paginator } from './Paginator'
-<<<<<<< HEAD
 export { default as PropertiesGrid } from './PropertiesGrid'
-=======
 export { default as withDataTable } from './withDataTable'
->>>>>>> 8d44ae9c51fd2bf76f8321d22e55a9022dc06343
