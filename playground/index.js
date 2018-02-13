8d44ae9c51fd2bf76f8321d22e55a9022dc06343
gffuma <fumagalli.gf@gmail.com>
2018-02-13 13:02:22 +0100

    withDataTable higher order component #3

--- playground/index.js
+++ playground/index.js
@@ -4,17 +4,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TopbarExample from './pages/TopbarExample'
 import PaginatorExample from './pages/PaginatorExample'
+import WithDataTableExample from './pages/WithDataTableExample'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const App = () => (
-    <Router basename={process.env.NODE_ENV === 'production' ? '/react-bootstrap-widgets/' : undefined}>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/topbar' exact component={TopbarExample} />
-        <Route path='/paginator' exact component={PaginatorExample} />
-      </Switch>
-    </Router>
-
+  <Router basename={process.env.NODE_ENV === 'production' ? '/react-bootstrap-widgets/' : undefined}>
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/Topbar' exact component={TopbarExample} />
+      <Route path='/Paginator' exact component={PaginatorExample} />
+      <Route path='/withDataTable' exact component={WithDataTableExample} />
+    </Switch>
+  </Router>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
